699089f463ff017e6f1a0c2c7a1107cb0fea2f8c
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-01 12:13:03 -0500

    fix(fetchUniquePastEvents): fixed function name

--- src/data/fetchUniquePastEvents.ts
+++ src/data/fetchUniquePastEvents.ts
@@ -6,7 +6,7 @@ export type PastEvent = {
   event_date: string;
 };
 
-export async function fetchUpcomingEvents(signal: AbortSignal) {
+export async function fetchPastEvents(signal: AbortSignal) {
     const { data, error } = await supabase
         .from("past_events_results_view")
         .select("*")
