48ac997c70f368326c4c563317c9204273f65554
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-01 14:32:13 -0500

    feat: ResultsPage

---
+++ src/pages/ResultsPage.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import { useParams, useLocation, Link } from "react-router-dom";
+// import {fetchPredictionsByEvent, type PredictedFight} from "../data/fetchPredictions";
+export default function ResultsPage() {
+    return (
+        <section>
+            <p>MEOW?</p>
+        </section>
+    )
+}
