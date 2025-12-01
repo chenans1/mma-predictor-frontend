f213e0c213695c3dfa866321e3a1c85dff3ff68b
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-01 16:13:32 -0500

    feat: added back button to resultspage

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation, Link } from "react-router-dom";
-// import {fetchPredictionsByEvent, type PredictedFight} from "../data/fetchPredictions";
+import {fetchPredictionsByEvent, type PredictedFight} from "../data/fetchPredictions";
+
 export default function ResultsPage() {
+    const { event_id } = useParams<{ event_id: string }>();
+    const [rows, setRows] = useState<PredictedFight[]>([]);
+    const [status, setStatus] = useState<"idle"|"loading"|"error"|"ready">("idle");
+    const [error, setError] = useState<string | null>(null);
+
+    
     return (
         <section>
+            <Link to="/" style={{ display: "inline-block", marginBottom: "1rem" }}>← Back</Link>
             <p>MEOW?</p>
         </section>
     )
